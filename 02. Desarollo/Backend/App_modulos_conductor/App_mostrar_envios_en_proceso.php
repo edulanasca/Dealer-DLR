1f7ff8e44ee4e0381898dac76ff29454f22b362f
Tito <titoyanac@gmail.com>
2021-06-21 19:17:48 -0500

    Add files via upload

---
+++ 02. Desarollo/Backend/App_modulos_conductor/App_mostrar_envios_en_proceso.php
@@ -0,0 +1,29 @@
+<?php 
+
+include_once("config.php");
+
+if ( isset($_POST['id_Conductor'] ) and $_POST['id_Conductor']!="") {
+    
+        $id_Conductor = mysqli_real_escape_string($db,$_POST['id_Conductor']);
+        $sql = " CALL usp_App_Conductor_Envios_En_Proceso ( '$id_Conductor' ) ";
+        $result = mysqli_query( $db , $sql);
+        
+        
+        if($result){
+            $count = mysqli_num_rows($result);
+            if( $count > 0 ){
+                $data = array();
+                while($row = mysqli_fetch_array($result)){
+                    array_push($data,$row);
+                }
+                mysqli_close($db); 
+                echo json_encode($data);
+            }
+        }else{
+            echo 'error';
+        }
+        
+        
+}
+
+?>
